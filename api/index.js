783635be49dff1d77509c77c8bb1bdf66338c52d
Henry Christoffer <henrycmeen@me.com>
2025-01-27 11:20:40 +0100

    Fix dependencies and clean up code

--- api/index.js
+++ api/index.js
@@ -1,15 +1,7 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Single connection promise with optimized settings
 // Single connection promise
 let clientPromise;
 
